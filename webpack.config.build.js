cbd109d7b269a7a524d3b9883a858e7f834c6044
katjam <katja@neontribe.co.uk>
2020-08-22 18:50:38 +0100

    Add icon, service worker and manifest stubs for pwa

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: {
@@ -18,10 +19,31 @@ module.exports = {
 
   plugins: [
     new CopyWebpackPlugin({
-        patterns: [
-            { from: './src/assets', to: './'},
-        ]
+      patterns: [
+        { from: './src/assets', to: './'},
+      ]
     }),
+    new SWPrecacheWebpackPlugin({
+      cacheId: 'vector-app',
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: 'service-worker.js',
+      minify: false,
+      navigateFallback: 'index.html',
+      staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/]
+    }),
+    new WebpackPwaManifest({
+      name: '[cCc] Vector app',
+      short_name: '[cCc] Vector app',
+      description: '[cCc] Vector app description',
+      background_color: '#ffffff',
+      theme_color: '#000000',
+      start_url: '/',
+      icons: [{
+        src: path.resolve('src/assets/icon.png'),
+        sizes: [192],
+        destination: path.join('static', 'img')
+      }]
+    })
   ],
 
   module: {
