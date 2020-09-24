e8a5e22fc3f1ea24a35c2c5a0fd9dcce9a1b1a4d
Nikomus <nick.wade@live.co.uk>
2020-09-24 11:33:21 +0100

    fix PlayerTeam message type

--- src/View/Messages.elm
+++ src/View/Messages.elm
@@ -17,7 +17,7 @@ view : Html Msg
 view =
     ul [ class "message-list p-0" ]
         [ renderMessage AL "Fish are cool"
-        , renderMessage Player "They sure are."
+        , renderMessage PlayerTeam "They sure are."
         , renderMessage AL "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         ]
 
