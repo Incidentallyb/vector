3251e7d28a7711f3ca90e1789497a5c2e08e6087
Kris Sum <kris@switchsystems.co.uk>
2020-09-28 16:48:21 +0100

    #34 adding score mechanic

--- src/GameData.elm
+++ src/GameData.elm
@@ -1,4 +1,4 @@
-module GameData exposing (GameData, filterMessages, init)
+module GameData exposing (GameData, filterMessages, init, updateSuccessScore, updateEconomicScore)
 
 import Content exposing (MessageData)
 import Dict exposing (Dict)
@@ -16,18 +16,17 @@ type alias GameData =
 init : GameData
 init =
     -- Start button on intro screen will eventually set this "init"
-    { choices = [ "init" ]
+    { choices = [ ]
     , teamName = "?"
     , scoreSuccess = 0
-    , scoreEconomic = 18000000
+    , scoreEconomic = 0
     , scoreHarm = 0
     }
 
 
 triggeredByChoices : String -> List String -> Bool
 triggeredByChoices currentChoices triggeredByList =
-    -- "init" only for the initial content after that all trigger strings start with "start"
-    List.member (String.replace "init|" "" currentChoices) triggeredByList
+    List.member currentChoices triggeredByList
 
 
 filterMessages : Dict String MessageData -> List String -> Dict String MessageData
@@ -38,3 +37,20 @@ filterMessages allMessages choices =
             String.join "|" (List.reverse choices)
     in
     Dict.filter (\key value -> triggeredByChoices choiceString value.triggered_by) allMessages
+
+
+applySuccessScore: Int -> List Content.MessageData -> Int
+applySuccessScore initialScore messages = 
+    List.foldl (+) initialScore (List.map (\record -> Maybe.withDefault 0 record.scoreChangeSuccess) messages)
+    
+updateSuccessScore: Content.Datastore -> List String -> Int -> Int
+updateSuccessScore datastore choices initialScore = 
+    applySuccessScore initialScore (Dict.values (filterMessages datastore.messages choices))
+
+applyEconomicScore: Int -> List Content.MessageData -> Int
+applyEconomicScore initialScore messages = 
+    List.foldl (+) initialScore (List.map (\record -> Maybe.withDefault 0 record.scoreChangeEconomic) messages)
+
+updateEconomicScore: Content.Datastore -> List String -> Int -> Int
+updateEconomicScore datastore choices initialScore = 
+    applyEconomicScore initialScore (Dict.values (filterMessages datastore.messages choices))
