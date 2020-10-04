996bbef387ced0ed8d6406083c57cd3108f235b9
katjam <katja@neontribe.co.uk>
2020-10-04 19:03:26 +0100

    bugfix: Key message and email Dict by triggering choice

--- src/View/Messages.elm
+++ src/View/Messages.elm
@@ -44,8 +44,8 @@ choiceStringsToButtons buttonString =
 view : GameData -> Dict String Content.MessageData -> Html Msg
 view gamedata messagesDict =
     ul [ class "message-list p-0" ]
-        (List.reverse
-            (Dict.values (filterMessages messagesDict gamedata.choices))
+        (Dict.values
+            (filterMessages messagesDict gamedata.choices)
             |> List.map (renderMessageAndPrompt gamedata.choices)
         )
 
