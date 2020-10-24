411fffbf00cbf0303e7f1a8455525d31f4f3cfc8
katjam <katja@neontribe.co.uk>
2020-10-24 15:28:49 +0100

    Basic styles for checkbox buttons

--- src/View/ChoiceButtons.elm
+++ src/View/ChoiceButtons.elm
@@ -1,4 +1,4 @@
-module View.ChoiceButtons exposing (ButtonInfo, choiceStringsToButtons, renderButtons, renderCheckboxes)
+module View.ChoiceButtons exposing (choiceStringsToButtons, renderButtons, renderCheckboxes)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -8,6 +8,13 @@ import Message exposing (Msg(..))
 
 type alias ButtonInfo =
     { label : String
+    , action : String
+    }
+
+
+type alias ButtonWithHeading =
+    { heading : String
+    , description : String
     , action : String
     }
 
@@ -33,39 +40,51 @@ choiceStringsToButtons buttonString =
     { label = parsedString, action = action }
 
 
+buttonWithHeadingFromButtonInfo : String -> ButtonInfo -> ButtonWithHeading
+buttonWithHeadingFromButtonInfo splitter info =
+    let
+        labelParts =
+            String.split splitter info.label
+    in
+    -- The part before the splitter
+    { heading = Maybe.withDefault "" (List.head labelParts)
+
+    -- The part after the splitter
+    , description = Maybe.withDefault "" (List.head (List.reverse labelParts))
+    , action = info.action
+    }
+
+
+renderCheckbox : ButtonWithHeading -> Html Msg
+renderCheckbox info =
+    -- Check markup for a11y
+    button
+        [ class "btn checkbox-button"
+        , if True then
+            onClick (ChoiceButtonClicked info.action)
+
+          else
+            Html.Attributes.class ""
+        ]
+        [ span [] [ text info.heading ], span [] [ text info.description ] ]
+
+
 renderCheckboxes : List ButtonInfo -> String -> Html Msg
 renderCheckboxes buttonList chosenValue =
     let
         submitValue =
             "two-extras"
 
-        -- Transform the button text
+        -- Transform the button text to contain headings
         splitButtonText =
-            List.map
-                (\choiceText ->
-                    Tuple.pair
-                        (Maybe.withDefault "" (List.head (String.split " - " choiceText.label)))
-                        (Maybe.withDefault "" (List.head (List.reverse (String.split " - " choiceText.label))))
-                )
-                buttonList
+            List.map (\choiceText -> buttonWithHeadingFromButtonInfo " - " choiceText) buttonList
     in
     div []
         [ div []
-            (List.map
-                (\( heading, description ) ->
-                    button
-                        [ class "btn"
-                        , if chosenValue == "" then
-                            onClick (ChoiceButtonClicked submitValue)
-
-                          else
-                            Html.Attributes.class ""
-                        ]
-                        [ span [] [ text heading ], span [] [ text description ] ]
-                )
-                splitButtonText
-            )
-        , button
+            -- The multiple choices
+            (List.map (\checkboxInfo -> renderCheckbox checkboxInfo) splitButtonText)
+        , -- The chooser submit value
+          button
             [ classList
                 [ ( "btn choice-button", True )
                 , ( "btn-primary", chosenValue == "" )
