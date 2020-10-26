10c7255bc6175003ad5297f5c1d2946b10b6a0f9
Nikomus <nick.wade@live.co.uk>
2020-10-26 15:01:58 +0000

    improve score display functions

--- src/View/Messages.elm
+++ src/View/Messages.elm
@@ -20,57 +20,48 @@ view : GameData -> Content.Datastore -> Html Msg
 view gamedata messagesDict =
     ul [ class "message-list p-0" ]
         (Dict.values
-            --     (filterMessages messagesDict.messages gamedata.choices)
-            --     |> List.map (renderMessageAndPrompt gamedata messagesDict gamedata.teamName)
-            -- )
-            (filterMessages messagesDict gamedata.choices)
-            |> List.map (renderMessageAndPrompt gamedata.choices gamedata.checkboxSet gamedata.teamName)
+            (filterMessages messagesDict.messages gamedata.choices)
+            |> List.map (renderMessageAndPrompt gamedata gamedata.choices gamedata.checkboxSet gamedata.teamName)
         )
 
 
-renderMessageAndPrompt : GameData -> Content.Datastore -> String -> Content.MessageData -> Html Msg
-renderMessageAndPrompt gamedata content team message =
+renderMessageAndPrompt : GameData -> List String -> CheckboxData -> String -> Content.MessageData -> Html Msg
+renderMessageAndPrompt gamedata choices checkboxes team message =
+    let
+        dummy =
+            Debug.log "Triggered by:" message.triggered_by
+
+        triggerList =
+            Maybe.withDefault "" (List.head message.triggered_by)
+    in
     li []
         [ div [ class "typing-indicator" ] [ span [] [ text "" ], span [] [ text "" ], span [] [ text "" ] ]
-        , renderMessage message.author message.content
-        , renderPrompt message gamedata.choices team
-        , if List.length message.triggered_by == 2 || List.length message.triggered_by == 5 then
-            renderScore "AL" gamedata content message
+        , if isScoreTime triggerList then
+            renderScore "AL" gamedata triggerList team
 
           else
             text ""
-        ]
-
-
-
--- getChoicePath : List String -> List String -> List String
--- getChoicePath gameChoices triggers =
---     List.head Maybe.withDefault "" (List.partition (\x -> x == "hi") [ "hi", "something  " ])
-
-
-renderScore : String -> GameData -> Content.Datastore -> Content.MessageData -> Html Msg
-renderScore from currentGameData content message =
-    div
-        [ class "message al w-75 float-left mt-3 ml-3 py-2" ]
-        [ div [ class "mx-3" ]
-            [ p [ class "message-from m-0" ] [ text from ]
-
-            -- use the message to partition all the choices and display score up to there
-            , div [] [ text ("Something " ++ String.fromInt (Maybe.withDefault 0 (List.head (GameData.displayScoreNow content currentGameData)))) ]
-            , p [] [ text "Your choices have produced the following results:" ]
-            , p [] [ text ("Success: " ++ String.fromInt currentGameData.scoreSuccess ++ "%") ]
-            , p [] [ text ("Economic: £" ++ String.fromInt currentGameData.scoreEconomic ++ " remaining") ]
-            , p [] [ text ("Harm: " ++ String.fromInt currentGameData.scoreHarm) ]
-            ]
-        ]
-
-
-renderMessageAndPrompt2 : List String -> CheckboxData -> String -> Content.MessageData -> Html Msg
-renderMessageAndPrompt2 choices checkboxes team message =
-    li []
-        [ div [ class "typing-indicator" ] [ span [] [ text "" ], span [] [ text "" ], span [] [ text "" ] ]
         , renderMessage message.author message.content
         , renderPrompt message choices checkboxes team
+        ]
+
+
+renderScore : String -> GameData -> String -> String -> Html Msg
+renderScore from currentGameData triggers team =
+    let
+        triggerDepth =
+            String.fromInt (List.length (filterChoiceString triggers))
+    in
+    div
+        [ class ("message al w-75 float-left mt-3 ml-3 py-2 triggers-" ++ triggerDepth) ]
+        [ div [ class "mx-3" ]
+            [ p [ class "message-from m-0" ] [ text from ]
+            , p [] [ text (t WellDone ++ team) ]
+            , p [] [ text (t Results) ]
+            , p [] [ text ("Success: " ++ String.fromInt currentGameData.scoreSuccess ++ "%") ]
+            , p [] [ text ("Economic: £" ++ String.fromInt currentGameData.scoreEconomic ++ ",000,000 remaining") ]
+            , p [] [ text ("Harm: " ++ String.fromInt currentGameData.scoreHarm) ]
+            ]
         ]
 
 
@@ -121,3 +112,25 @@ renderPrompt message choices checkboxes team =
 
     else
         text ""
+
+
+isScoreTime : String -> Bool
+isScoreTime triggers =
+    let
+        debugFilteredList =
+            Debug.log "Filtered list: " filterChoiceString triggers
+    in
+    if List.length (filterChoiceString triggers) == 2 || List.length (filterChoiceString triggers) == 4 || List.length (filterChoiceString triggers) == 5 then
+        True
+
+    else
+        False
+
+
+filterChoiceString : String -> List String
+filterChoiceString input =
+    let
+        genericWords =
+            [ "init", "start", "step", "change" ]
+    in
+    List.filter (\item -> not (List.member item genericWords)) (String.split "|" input)
