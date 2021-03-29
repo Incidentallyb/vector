04c1ef8132d5a31f449540b2b02a4e1a317bd008
katjam <katjamordaunt@gmail.com>
2021-03-29 18:37:16 +0100

    Add styles and logos to end

--- src/View/EndInfo.elm
+++ src/View/EndInfo.elm
@@ -11,16 +11,22 @@ import Route exposing (Route(..))
 
 view : Html Msg
 view =
-    div [ class "intro container-fluid" ]
-        [ div [ class "row my-4" ]
-            [ h1 [ class "d-block mx-auto" ]
-                [ text (t EndInfoHeader)
+    div [ class "end container-fluid" ]
+        [ div [ class "container" ]
+            [ div [ class "row my-4" ]
+                [ h1 [ class "d-block mx-auto" ]
+                    [ text (t EndInfoHeader)
+                    ]
+                ]
+            , div [ class "row my-4" ]
+                [ p [] [ Markdown.toHtml [] (t EndInfoParagraph1) ]
+                , p [] [ text (t EndInfoParagraph2) ]
+                , p [] [ Markdown.toHtml [] (t EndInfoParagraph3) ]
+                , p [] [ text (t EndInfoParagraph4) ]
                 ]
             ]
-        , div [ class "row my-4" ]
-            [ p [] [ Markdown.toHtml [] (t EndInfoParagraph1) ]
-            , p [] [ text (t EndInfoParagraph2) ]
-            , p [] [ Markdown.toHtml [] (t EndInfoParagraph3) ]
-            , p [] [ text (t EndInfoParagraph4) ]
+        , div [ class "row my-4 logos" ]
+            [ div [ class "col-md-6" ] [ img [ src "images/wellcome-logo.png" ] [] ]
+            , div [ class "col-md-6" ] [ img [ src "images/AnNex_Logo_600.png" ] [] ]
             ]
         ]
